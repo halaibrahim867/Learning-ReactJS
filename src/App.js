f1b245c34ad533be35b751e2dc4115d628ccc59e
Hala Ibrahim <halaibrahim867@gmail.com>
2026-02-02 09:45:08 +0200

    add array state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-import logo from "./logo.svg";
 import "./App.css";
-import MyForm from "./MyForm";
+import { useState } from "react";
+
 function App() {
+  const [deviceName, setDeviceName] = useState("");
+  const [devices, setDevices] = useState([
+    { id: 1, name: "Laptop" },
+    { id: 2, name: "Smartphone" },
+    { id: 3, name: "Tablet" },
+  ]);
+
+  const deviceList = devices.map((device) => {
+    return (
+      <li key={device.id}>
+        {device.name}{" "}
+        <button
+          onClick={() => {
+            handleEditItem(device.id);
+          }}
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => {
+            handleDeleteItem(device.id);
+          }}
+        >
+          Delete
+        </button>
+      </li>
+    );
+  });
+
+  function handleAddClick() {
+    setDevices([...devices, { id: devices.length + 1, name: deviceName }]);
+    setDeviceName("");
+  }
+
+  function handleDeleteItem(id) {
+    const newDevices = devices.filter((device) => device.id !== id);
+    setDevices(newDevices);
+  }
+
+  function handleEditItem(id) {
+    const newDevices = devices.map((device) => {
+      if (device.id === id) {
+        return { ...device, name: "Edited " + device.name };
+      }
+      return device;
+    });
+    setDevices(newDevices);
+  }
   return (
     <div className="App">
-      <MyForm />
+      {deviceList}
+
+      <div>
+        <input
+          placeholder="Enter device name"
+          value={deviceName}
+          onChange={(e) => setDeviceName(e.target.value)}
+        />
+        <button onClick={handleAddClick}>Add Device</button>
+      </div>
     </div>
   );
 }
